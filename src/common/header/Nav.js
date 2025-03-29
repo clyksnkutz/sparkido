515db66c9cc1eb16934087e5abef26506f59085b
joshua <joshua@joshuas-MacBook-Pro.local>
2025-03-29 16:29:11 +0530

    nav bar changes

--- src/common/header/Nav.js
+++ src/common/header/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaAngleDown } from "react-icons/fa";
 
 
 const Nav = () => {
@@ -8,36 +7,36 @@ const Nav = () => {
         <nav className="mainmenu-nav">
             <ul className="mainmenu">
                 <li className="menu-item-has-children">
-                    <Link to="#">Digital Agency <FaAngleDown /> </Link>
-                    <ul className="axil-submenu">
+                    <Link to="/">Home  </Link>
+                    {/* <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/digital-agency"}>Digital Agency</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/creative-agency"}>Creative Agency</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/personal-portfolio"}>Personal Portfolio</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/home-startup"}>Home Startup</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/corporate-agency"}>Corporate Agency</Link></li>
                         <li><a href="https://new.axilthemes.com/demo/react/abstrak-rtl/">RTL Demo</a></li>
-                    </ul>
+                    </ul> */}
                 </li>
                 <li className="menu-item-has-children">
-                    <Link to="#">Services <FaAngleDown /></Link>
-                    <ul className="axil-submenu">
+                    <Link to="/service-details/design">ATL </Link>
+                    {/* <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/service-one"}>Service</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/service-two"}>Service Two</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/service-details/design"}>Service Details</Link></li>
-                    </ul>
+                    </ul> */}
                 </li>
                 <li className="menu-item-has-children">
-                    <Link to="#">Portfolio <FaAngleDown /></Link>
-                    <ul className="axil-submenu">
+                    <Link to="/home-startup">Smart Panel </Link>
+                    {/* <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-one"}>Two Column</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-two"}>Three Column</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-three"}>Four Column</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-width-one"}>Three Column Width</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-width-two"}>Four Column Width</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-details/plan-management"}>Portfolio Details</Link></li>
-                    </ul>
+                    </ul> */}
                 </li>
-                <li className="menu-item-has-children">
+                {/* <li className="menu-item-has-children">
                     <Link to="#">Pages <FaAngleDown /></Link>
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/about-us"}>About Us</Link></li>
@@ -53,18 +52,18 @@ const Nav = () => {
                         <li><Link to={process.env.PUBLIC_URL + "/404"}>404 Page</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/coming-soon"}>Coming Soon</Link></li>
                     </ul>
-                </li>
-                <li className="menu-item-has-children">
-                    <Link to="#">Blog <FaAngleDown /></Link>
+                </li> */}
+                {/* <li className="menu-item-has-children">
+                    <Link to="#">Contact <FaAngleDown /></Link>
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/blog-grid"}>Blog</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/blog-details/1"}>Standard Post</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/blog-details/2"}>Gallery Post</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/blog-details/3"}>Video Post</Link></li>
-                        {/* <li><Link to={process.env.PUBLIC_URL + "#"}>Audio Post</Link></li>
-                        <li><Link to={process.env.PUBLIC_URL + "#"}>Quote Post</Link></li> */}
+                        <li><Link to={process.env.PUBLIC_URL + "#"}>Audio Post</Link></li>
+                        <li><Link to={process.env.PUBLIC_URL + "#"}>Quote Post</Link></li>
                     </ul>
-                </li>
+                </li> */}
                 <li><Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link></li>
             </ul>
         </nav>
