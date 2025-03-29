c420ece8e5a87e623499b2fbf991dde59593dc1a
joshua <joshua@joshuas-MacBook-Pro.local>
2025-03-29 17:34:35 +0530

    ATL page

--- src/pages/ServiceDetails.js
+++ src/pages/ServiceDetails.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import Tilty from 'react-tilty';
 import FooterOne from '../common/footer/FooterOne';
 import HeaderOne from '../common/header/HeaderOne';
-import BcrumbBannerOne from '../elements/breadcrumb/BcrumbBannerOne';
-import CtaLayoutOne from '../component/cta/CtaLayoutOne';
-import SectionTitle from '../elements/section-title/SectionTitle';
-import ProjectPropOne from '../component/project/itemProp/ProjectPropOne';
-import { slugify } from '../utils';
-import ServiceData from "../data/service/ServiceMain.json";
-import ProjectData from "../data/project/ProjectData.json";
-import AboutTwo from '../component/about/AboutTwo';
-import ColorSwitcher from '../elements/switcher/ColorSwitcher';
 import SEO from '../common/SEO';
-import Tilty from 'react-tilty';
+import CtaLayoutOne from '../component/cta/CtaLayoutOne';
+import ProjectPropOne from '../component/project/itemProp/ProjectPropOne';
+import ProjectData from "../data/project/ProjectData.json";
+import ServiceData from "../data/service/ATLSolutions.json";
+import BcrumbBannerOne from '../elements/breadcrumb/BcrumbBannerOne';
+import SectionTitle from '../elements/section-title/SectionTitle';
+import ColorSwitcher from '../elements/switcher/ColorSwitcher';
 
 const allServiceData = ServiceData;
 const getProjectData = ProjectData;
 
 const ServiceDetails = () => {
 
-    const params = useParams();
-    const serviceSlug = params.slug;
+    // const params = useParams();
+    // const serviceSlug = params.slug;
 
-    const getServiceData = allServiceData.filter(data => slugify(data.title) === serviceSlug);
+    // const getServiceData = allServiceData.filter(data => slugify(data.title) === serviceSlug);
+    const getServiceData = allServiceData.filter(data => data.title);
     const detailsService = getServiceData[0];
 
+    
     return (
         <>
         <SEO title="Service Details" />
         <ColorSwitcher />
         <main className="main-wrapper">
             <HeaderOne />
-            <BcrumbBannerOne 
+            <BcrumbBannerOne
             title={detailsService.title}
             paragraph ={detailsService.description}
             styleClass=""
-            mainThumb="/images/banner/banner-thumb-4.png"
+            mainThumb="/images/banner/atal.png"
             />
-            <AboutTwo />
+            {/* <AboutTwo /> */}
            
             <div className="section section-padding bg-color-light pb--70">
                 <SectionTitle 
@@ -74,7 +73,7 @@ const ServiceDetails = () => {
             </div>
             <div className="section section-padding">
                 <div className="container">
-                    <SectionTitle 
+                    <SectionTitle
                         subtitle="Our Project"
                         title="Featured Designs"
                         description=""
