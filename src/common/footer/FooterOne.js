e97e7c270da8037015b41a3507dece42b8ad0c72
SamStar3 <samstar1044948@gmail.com>
2025-03-29 17:18:23 +0530

    footerOne updated

--- src/common/footer/FooterOne.js
+++ src/common/footer/FooterOne.js
@@ -12,7 +12,7 @@ const FooterOne = ({parentClass}) => {
     return (
         <footer className={`footer-area ${parentClass}`}>
             <div className="container">
-                <div className="footer-top">
+                {/* <div className="footer-top">
                     <div className="footer-social-link">
                         <ul className="list-unstyled">
                             <li><Link to="https://facebook.com/"><FaFacebookF /></Link></li>
@@ -26,7 +26,7 @@ const FooterOne = ({parentClass}) => {
                             <li><Link to="https://www.tiktok.com/en/"><FaTiktok /></Link></li>
                         </ul>
                     </div>
-                </div>
+                </div> */}
                 <div className="footer-main">
                     <div className="row">
                         <div className="col-xl-6 col-lg-5">
@@ -46,40 +46,49 @@ const FooterOne = ({parentClass}) => {
                         </div>
                         <div className="col-xl-6 col-lg-7">
                             <div className="row">
-                                <div className="col-sm-6">
+                                <div className="col-sm-4">
                                     <div className="footer-widget">
-                                        <h6 className="widget-title">Services</h6>
+                                        <h6 className="widget-title">Address</h6>
                                         <div className="footer-menu-link">
-                                            <ul className="list-unstyled">
+                                            {/* <ul className="list-unstyled">
                                                 {getServiceData.slice(0, 6).map((data, index) => (
                                                     <li key={index}>
                                                         <Link to={process.env.PUBLIC_URL + `/service-details/${slugify(data.title)}`}>{data.title}</Link>
                                                     </li>
                                                 ))}
-                                            </ul>
+                                            </ul> */}
+
+                                            <p>442, South 2nd Main Road, Kapaleeshwarar Nagar, Neelangarai, Chennai - 600 115</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-sm-3">
+                                <div className="col-sm-5">
                                     <div className="footer-widget">
-                                        <h6 className="widget-title">Resourses</h6>
+                                        <h6 className="widget-title">Contact</h6>
                                         <div className="footer-menu-link">
-                                            <ul className="list-unstyled">
+                                            {/* <ul className="list-unstyled">
                                                 <li><Link to={process.env.PUBLIC_URL + "/blog-grid"}>Blog</Link></li>
                                                 <li> <Link to={process.env.PUBLIC_URL + "/case-study"}>Case Studies</Link></li>
                                                 <li><Link to={process.env.PUBLIC_URL + "/project-grid-one"}>Portfolio</Link></li>
-                                            </ul>
+                                            </ul> */}
+                                            {/* <p>+91 94445 25095   smartkriya.edu@gmail.com     www.smartkriya.com</p> */}
+                                            <p>
+                                                <span>+91 94445 25095</span> 
+                                                <a href="mailto:smartkriya.edu@gmail.com">smartkriya.edu@gmail.com</a>
+                                                <a href="https://www.smartkriya.com" target="_blank" rel="noopener noreferrer">www.smartkriya.com</a>
+                                            </p>
+
                                         </div>
                                     </div>
                                 </div>
                                 <div className="col-sm-3">
                                     <div className="footer-widget">
-                                        <h6 className="widget-title">Support</h6>
+                                        <h6 className="widget-title">Pages</h6>
                                         <div className="footer-menu-link">
                                             <ul className="list-unstyled">
                                                 <li><Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link></li>
-                                                <li> <Link to={process.env.PUBLIC_URL + "/privacy-policy"}>Privacy Policy</Link></li>
-                                                <li><Link to={process.env.PUBLIC_URL + "/terms-use"}>Terms of Use</Link></li>
+                                                <li> <Link to={process.env.PUBLIC_URL + "/home-startup"}>Smart Panel</Link></li>
+                                                <li><Link to={process.env.PUBLIC_URL + "/service-details/design"}>ATL</Link></li>
                                             </ul>
                                         </div>
                                     </div>
